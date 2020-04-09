566a95851dbb25373d4d2d9df78757a2f5c6afad
Alexandre Amory <amamory@gmail.com>
2020-04-09 18:37:36 -0300

    mnist dma is working with data moves and pointers to the NN banks. assuming a single mult with a single data stream. app code loaded with commented debug messages.

--- extensions/orca-core/src/orca-core.c
+++ extensions/orca-core/src/orca-core.c
@@ -28,8 +28,8 @@
 //#include "../../../applications/mnist-ext-mult/include/mnist-ext-mult.h"
 //#include "../../../applications/mnist-ext-vet-mult/include/mnist-ext-vet-mult.h"
 //#include "../../../applications/mnist-ext-vet-seq-mult/include/mnist-ext-vet-seq-mult.h"
-//#include "../../../applications/mnist-ext-mult-dma/include/mnist-ext-mult-dma.h"
-#include "../../../applications/simple-dma/include/simple-dma.h"
+#include "../../../applications/mnist-ext-mult-dma/include/mnist-ext-mult-dma.h"
+//#include "../../../applications/simple-dma/include/simple-dma.h"
 
 //#include "../../../applications/cpp-mult/include/cpp-mult.h"
 //#include "../../../applications/ann/include/ann.h" 
@@ -57,8 +57,8 @@ void app_main(void)
 	//hf_spawn(mnist_ext_mult, 5, 4, 5, "mnist_ext_mult", 128 * 1024);   // 128 Kbytes !!!
 	//hf_spawn(mnist_ext_vet_mult, 5, 4, 5, "mnist_ext_vet_mult", 128 * 1024);   // 128 Kbytes !!
 	//hf_spawn(mnist_ext_vet_seq_mult, 5, 4, 5, "mnist_ext_vet_seq_mult", 256 * 1024); // 128 Kbytes !!
-	//hf_spawn(mnist_ext_mult_dma, 5, 4, 5, "mnist_ext_mult_dma", 256 * 1024); // 128 Kbytes !!
-	hf_spawn(simple_dma, 5, 4, 5, "simple_dma", 1024);
+	hf_spawn(mnist_ext_mult_dma, 5, 4, 5, "mnist_ext_mult_dma", 256 * 1024); // 128 Kbytes !!
+	//hf_spawn(simple_dma, 5, 4, 5, "simple_dma", 1024);
 	//hf_spawn(int_mult, 5, 4, 5, "int_mult", 1024);   //10%
 	//hf_spawn(float_mult, 5, 4, 5, "float_mult", 1024);   //10%
 	//hf_spawn(cpp_mult, 5, 4, 5, "cpp_mult", 1024);   //10%
