efb25ea7eb6f163aff05e5ae0adf2448da6b5e01
Anderson Domingues <ti.andersondomingues@gmail.com>
2020-09-16 15:40:59 -0300

    simplified test for new 32-bit ni driver

--- applications/dma-driver-test/src/dma-driver-test.c
+++ applications/dma-driver-test/src/dma-driver-test.c
@@ -8,33 +8,28 @@ int main(){
 
 	dma_init();
 
-	//periodically bursts messages through the network 
-	while(1){
+	uint32_t x = 0;
+	uint32_t y = 0;
+	uint32_t size = 10;
 
-		//holds until data is available from the 
-		//network
-		// while(!dma_recv_probe());
-
-		uint32_t x = 0;
-		uint32_t y = 0;
-		uint32_t size = 10;
-
-		for(int i = 0; i < 10; i++){
-			buffer[i] = i + 10;
-		}
-
-		//push data back to the network
-		dma_send_start(x, y, buffer, size);
-
-		while(dma_recv_probe()){
-			dma_recv_start(&x, &y, &size, buffer);
-		}
-
-		for(int i = 0; i < 10; i++){
-			buffer[i] = buffer[i] + 1;
-		}
-
-		//push data back to the network
-		dma_send_start(x, y, buffer, size);
+	for(int i = 0; i < 10; i++){
+		buffer[i] = i + 10;
 	}
+
+	//push data back to the network
+	dma_send_start(x, y, buffer, size);
+
+	while(!dma_recv_probe());
+	
+	dma_recv_start(&x, &y, &size, buffer);
+	
+	for(int i = 0; i < 10; i++){
+		buffer[i] = buffer[i] + 1;
+	}
+
+	//push data back to the network
+	dma_send_start(x, y, buffer, size);
+
+	//SYSCALL
+	return 0;
 }
